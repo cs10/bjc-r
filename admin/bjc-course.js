cf2bc8d1f09f169787497657bd99c2c81517327a
Peter Sujan <peterasujan@gmail.com>
2013-09-02 19:52:15 -0700

    Can now hide videos/readings/etc from entire (or parts of) courses. See <div class='topic_container'> in berkeley_bjc.html for an example.

---
+++ admin/bjc-course.js
@@ -0,0 +1,41 @@
+/*
+ * bjc-course.js
+ * 
+ * loaded in course pages.
+ * can depend on jquery and bjc-library.js
+ */
+
+ /* hide items and pass on course name. */
+bjc.editURLs = function() {
+    $(".topic_container").each(function() {
+        var args = "";
+        var attributes = this.attributes;
+        for (var i = 0; i < attributes.length; i++) {
+            if (attributes[i].name != "class") {
+                args += "&" + attributes[i].name + "=" + attributes[i].value;
+            }
+        }
+        $(this).find(".topic_link").each(function(){
+            $(this).find("a")[0].href = $(this).find("a")[0].href + args;
+        });
+    });
+    $("a").each(function() {
+        this.href = this.href + "&course=" + document.location.href.split("?")[0].split("/").pop();
+    });
+}
+
+bjc.addTitle = function() {
+    // insert main div
+    if ($("#full").length == 0) {
+        $(document.body).wrapInner('<div id="full"></div>');
+	}
+
+    // add header div
+	$("#full").prepend($(document.createElement("div")).attr({"class":"header"}).html(document.title));
+
+}
+ 
+ $(document).ready(function() {
+    bjc.editURLs();
+    bjc.addTitle();
+ });
