cc3bbd974cfa2a185a3e56ea12aea98de958824d
brianharvey <bh@cs.berkeley.edu>
2015-07-03 11:02:16 -0700

    Michael's library fix.

--- llab/script/library.js
+++ llab/script/library.js
@@ -20,6 +20,7 @@ llab.CORSCompliantServers.push("bjc.eecs.berkeley.edu");
 llab.CORSCompliantServers.push("snap.berkeley.edu");
 llab.CORSCompliantServers.push("inst.eecs.berkeley.edu");
 llab.CORSCompliantServers.push("cs10.berkeley.edu");
+llab.CORSCompliantServers.push("bjc.edc.org");
 llab.CORSCompliantServers.push("localhost");
 llab.CORSCompliantServers.push("0.0.0.0");
 
@@ -41,9 +42,9 @@ llab.getSnapRunURL = function(targeturl) {
 	var finalurl = llab.snapRunURLBase;
 	var currdom = document.domain;
 	if (currdom == "localhost") {
-		// currdom = 'http://' + currdom + ":" + window.location.port;
+		 currdom = 'http://' + currdom + ":" + window.location.port;
 	}
-	if (llab.CORSCompliantServers.indexOf(currdom) == -1) {
+	else if (llab.CORSCompliantServers.indexOf(currdom) == -1) {
 		finalurl += llab.CORSproxy;
 	}
 	if (targeturl.indexOf("..") != -1 || targeturl.indexOf(llab.rootURL) == -1) {
