bd672e726d7fb966cb83e96a25d73992ec63906b
Steven Traversi <straversi@berkeley.edu>
2016-11-06 22:42:52 -0800

    include starter file

---
+++ prog/python/python2/lab2.py
@@ -0,0 +1,35 @@
+# Exercise 1
+def push_first_odd_back(lst):
+    pass
+
+# Exercise 2
+def flatten(lst):
+    return lst
+
+# Exercise 3.1
+def squares_of_evens(lst):
+    return lst
+
+# Exercise 3.2
+def nth_power_of_evens(lst, n):
+    return lst
+
+# Exercise 4
+def substitute_base(string, old, new):
+    return string
+
+# Exercise 5
+def combine(lst):
+    return ""
+
+# Exercise 6
+def base_freq(string):
+    return {}
+
+# Exercise 7.1
+def substitute_chars(string, replacements):
+    return string
+
+# Exercise 7.2
+def invert_dict(original):
+    return original
