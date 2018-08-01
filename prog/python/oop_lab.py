663f0733b6117f47ad10a0f2146a3ce40705bb66
Niket Kulkarni <niket.kul@berkeley.edu>
2018-08-01 00:59:20 -0700

    adding maxsons changes and starter file to OOP lab

---
+++ prog/python/oop_lab.py
@@ -0,0 +1,35 @@
+class Book:
+
+	def __init__(self, genre, title, author, publication_year):
+
+	def calculate_age(self):
+		#Calculates the age of the book by subtracting its publication year from the current year.
+		return
+
+	def outdated(self, old_age):
+		#Determines if the book is outdated. Takes in an old_age and sets worn_down to True.
+
+
+	def add_genre(self):
+		'''If the book's genre isn't in the varieties, then you'll need to add the genre to varieties
+		as well as the original book's information. Otherwise, just make sure the book isn't
+		inside varieties before you add it!'''
+
+
+
+class Textbook(Book):
+	publisher = 'Houghton Mifflin Harcourt'
+
+	def random_function(self):
+		return 'Do something'
+
+class UCBMFET:
+
+    def __init__(self, name):
+
+    def tag_ur_friend_in_meme(self, friend):
+    	#Hint: use the isinstance function
+
+    def post_in_UCBMFET(self, title_of_post):
+
+    def like_a_post_in_UCBMFET(self, title_of_post):
