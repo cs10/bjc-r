1633b4773a2d6c28d95aedf4f0145d0d312b35af
Sungjoo Jeon <sungjoojeon@berkeley.edu>
2020-11-01 11:31:12 -0800

    edited colorama_lab.py

--- colorama_lab.py
+++ colorama_lab.py
@@ -10,4 +10,28 @@ def change_text_color(text, color):
 	print(getattr(Fore, color) + text)
 
 """Question 2: Write a function that prints given text into a given background color"""
-	
+
+
+
+
+
+
+
+
+
+####### Teext Processing ########
+
+def read_file(filename):
+    """Returns the text contained in file with given filename."""
+    f = open(filename, "r")
+    text = f.read()
+    return text
+
+def func_name(var):
+	"""returns text changed colors w following conditionsn"""
+	return
+
+
+
+text = read_file("replace with file location ex. text/gettysburg.txt")
+print(apply_language_game(text, func_name))
