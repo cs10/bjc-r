271151f396447d6f48eff71348c6b628b77b0fb2
Rebecca <Rebecca@Rebeccas-MacBook-Pro-3.local>
2020-11-05 14:51:36 -0800

    almost finished skeleton

--- colorama_lab.py
+++ colorama_lab.py
@@ -5,21 +5,29 @@ from colorama import Fore, Back, Style
 """Question 1: Write a function that prints given text into a given color"""
 def change_text_color(text, color):
 
-"*** YOUR CODE HERE ***"
+	"*** YOUR CODE HERE ***"
 
 	print(getattr(Fore, color) + text)
+	print(Fore.RESET)
 
-"""Question 2: Write a function that prints given text into a given background color"""
+"""Question 2: Write a function that prints given text onto a given background color"""
+def change_background_color(text, color):
+	
+	"*** YOUR CODE HERE ***"
+
+	print(getattr(Back, color) + text)
+	print(Back.RESET)
 
 
+def change_brightness(text, level):
+"""Question 2: Write a function that prints given text in a given brightness level"""
+
+	"*** YOUR CODE HERE ***"
+
+	print(getattr(Style, level) + text)
 
 
-
-
-
-
-
-####### Text Processing ########
+####### Text Processing with Colorama ########
 
 def read_file(filename):
     """Returns the text contained in file with given filename."""
@@ -27,9 +35,21 @@ def read_file(filename):
     text = f.read()
     return text
 
-def func_name(var):
-	"""returns text changed colors w following conditionsn"""
-	return
+def parse_text(text):
+	"""Prints the given text with changed formatting according to different conditions"""
+
+	"*** YOUR CODE HERE ***"
+
+	text = list(text)
+	for word in text:
+		if len(word) >= :
+			change_
+		if 'y' in word:
+			change_
+		if word[len(word)-1] == 's':
+			change_
+		if is_palindrome(word):
+			change_
 
 
 
