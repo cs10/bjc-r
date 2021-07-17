db3da1f336c0958504694bcdbe24ffc8977e1f11
Ze-Ning <zeningong@berkeley.edu>
2021-07-16 23:26:59 -0600

    more indentation oops

--- prog/python/oop_lab2.py
+++ prog/python/oop_lab2.py
@@ -1,19 +1,19 @@
 class Book:
 	def __init__(self, genre, title, author, publication_year):
-		return 0
+		return 
 		
 	def calculate_age(self):
 		#Calculates the age of the book by subtracting its publication year from the current year.
-		return 0
+		return 
 	def outdated(self, old_age):
 		#Determines if the book is outdated. Takes in an old_age and sets worn_down to True.
-		return 0
+		return 
 
 	def add_genre(self):
 		'''If the book's genre isn't in the varieties, then you'll need to add the genre to varieties
 		as well as the original book's information. Otherwise, just make sure the book isn't
 		inside varieties before you add it!'''
-		return 0
+		return 
 
 class Textbook(Book):
 	publisher = 'Houghton Mifflin Harcourt'
@@ -23,11 +23,11 @@ class Textbook(Book):
 
 class MemePage:
 	def __init__(self, name):
-		return 0
+		return 
 	def tag_ur_friend_in_meme(self, friend):
-		return 0
+		return 
 		#Hint: use the isinstance function
 	def post_in_page(self, title_of_post):
-		return 0
+		return 
 	def like_a_post_in_page(self, title_of_post):
-		return 0
+		return 
